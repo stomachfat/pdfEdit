8ccc16ea2152814b14a45ea3b0cf2712bca76a4f
Jimmy <jimmy@myvisor.com>
2018-06-10 15:30:57 -0400

    testing something

--- config/production.env.js
+++ config/production.env.js
@@ -1,4 +1,4 @@
 module.exports =  {
-  stripePublishableKey: 'pk_live_zeQQ32YSVlP1qdK1oGbcoXe8',
-  stripeSecretKey: 'sk_live_VRbmwURAsPo3tXcY7n8c9iWb',
+  stripePublishableKey: 'pk_live_IenVZWlkUzACq1D0TF3hjOFc',
+  stripeSecretKey: 'sk_live_s5RVcICMn66VaJUao5o4Lqyd',
 };
